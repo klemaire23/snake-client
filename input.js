89626b162d64a73f6f579a540ccb11e343c95e75
Kamila Lemaire <kb.lemaire@gmail.com>
2023-04-12 17:26:25 -0600

    added keys to user input

--- input.js
+++ input.js
@@ -1,8 +1,9 @@
-const net = require("net");
+let connection;
 
 // setup interface to handle user input from stdin
 
-const setupInput = function() {
+const setupInput = (conn) => {
+  connection = conn
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -18,6 +19,22 @@ const handleUserInput = function() {
     if (key === '\u0003') {
       process.exit();
     }
+
+    if (key === 'w') {
+      connection.write("Move: up");
+    }
+
+    if (key === 'a') {
+      connection.write("Move: left");
+    }
+
+    if (key === 's') {
+      connection.write("Move: down");
+    }
+    if (key === 'd') {
+      connection.write("Move: right");
+    }
+
   });
 };
 
--- play.js
+++ play.js
@@ -3,7 +3,6 @@ const { connect } = require('../snake-client/client')
 const { setupInput } = require('../snake-client/input')
 
 console.log("Connecting ...");
-connect();
 
-setupInput();
+setupInput(connect());
 
