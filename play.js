448b9bee08a01703f424cfaf9dd9b4466638ddb1
Kamila Lemaire <kb.lemaire@gmail.com>
2023-04-12 16:49:22 -0600

    adding user inputs

--- play.js
+++ play.js
@@ -1,4 +1,27 @@
 const net = require("net");
-const {connect} = require('../snake-client/client')
+const { connect } = require('../snake-client/client')
+
+// setup interface to handle user input from stdin
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.on("data", handleUserInput);
+  stdin.resume();
+  return stdin;
+
+};
+
+const handleUserInput = function() {
+  const stdin = process.stdin;
+  stdin.on("data", (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+};
 
 connect();
+setupInput();
+
